d9a6c6d1496f7331ea3f9b29f02eba34e61b62c6
Todd Ransom <toddransom@me.com>
2024-07-22 10:50:32 -0400

    prep for release

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# create systemd service file
+    # need to do this here so paths can point to home (can't use env vars in systemd files)
+
+# install systemd service
+
+# enable and start systemd service
