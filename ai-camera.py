77154f92add0763c37429c39a165f34ae9470789
Todd Ransom <toddransom@me.com>
2024-12-31 14:19:45 -0500

    first working version

--- ai-camera.py
+++ ai-camera.py
@@ -1,35 +1,48 @@
 #!/usr/bin/env python3
-"""Use imx500 raspberry pi ai camera to look for events and record a poster image + video for as long as the event lasts."""
+"""Use imx500 raspberry pi ai camera to look for events and record a poster image + video
+ for as long as the event lasts."""
 from picamera2 import MappedArray, Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 from picamera2.devices import IMX500
 from picamera2.devices.imx500 import (NetworkIntrinsics,
                                       postprocess_nanodet_detection)
 import logging
 import sys
+import os
 import numpy as np
 import time
 import requests
+from functools import lru_cache
+import cv2
+
+last_detections = []
 
 
 # An event is an object detection that has not been specifically excluded by configuration
 # an event has a category, confidence, timestamp, and bounding box
 # an event should trigger an image capture
 # an event should trigger a video capture that lasts until only excluded objects are in frame
-# once the video capture concludes, the event should be sent to the local tinycam-ui API to tag the video and poster image
+# once the video capture concludes, the event should be sent to the local tinycam-ui API to tag 
+# the video and poster image
 # configuration options:
 # which model to use
 # API server
 # excluded objects
 # excluded regions
 
-## base on picamera2/examples/imx500/imx500_object_detection_demo.py
+## based on picamera2/examples/imx500/imx500_object_detection_demo.py
 
 MODEL = "/usr/share/imx500-models/imx500_network_ssd_mobilenetv2_fpnlite_320x320_pp.rpk"
+images_dir: str = os.getenv('IMAGES_DIR',".")
 API_SERVER = "http://localhost:5173"
 EXCLUDED_OBJECTS = []
 EXCLUDED_REGIONS = []
 
+encoder = H264Encoder(1000000)
+
 class Detection:
+    """Class to encapsulate a detection from the imx500"""
     def __init__(self, coords, category, conf, metadata):
         """Create a Detection object, recording the bounding box, category and confidence."""
         self.category = category
@@ -37,10 +50,54 @@ class Detection:
         self.box = imx500.convert_inference_coords(coords, metadata, picam2)
 
     def __repr__(self):
-        return f"{type(self).__name__}(category='{self.category}', confidence={self.conf})"
+        labels = get_labels()
+        return f"{type(self).__name__}(category='{labels[int(self.category)]}', confidence={self.conf:.2f})"
+    
+class Capture:
+    """A video capture with poster image"""
+    def __init__(self,asset_id):
+        self.asset_id = asset_id
+        self.is_capturing: bool = False
+        self.poster = None
+        self.tags: set = {}
+
+    def start(self):
+        ...
+
+    def stop(self):
+        # TODO: tag via api
+        picam2.stop_encoder()
+        logging.info("Stop recording")
+
+        ...
+
+    def continue_or_start(self,request,filestem):
+        if not self.is_capturing:
+            self.is_capturing = True
+            filestem = time.strftime("%Y%m%d-%H%M%S")
+            
+            # capture poster 
+            request.save("main", f"{images_dir}/{filestem}.jpg")
+            logging.info("Preview saved to %s.jpg", filestem)
+            
+            # start video capture
+            filename = f"{filestem}.mp4"
+            logging.info("Start recording to %s", filename)
+            encoder.output = FfmpegOutput(f"{images_dir}/{filename}")
+            picam2.start_encoder(encoder)
+            
+@lru_cache
+def get_labels():
+    labels = intrinsics.labels
+
+    if intrinsics.ignore_dash_labels:
+        labels = [label for label in labels if label and label != "-"]
+    return labels
+
 
 def parse_detections(metadata: dict):
     """Parse the output tensor into a number of detected objects, scaled to the ISP output."""
+    logging.debug('parse_detections')
     global last_detections
     bbox_normalization = intrinsics.bbox_normalization
     bbox_order = intrinsics.bbox_order
@@ -75,7 +132,6 @@ def parse_detections(metadata: dict):
     ]
     return last_detections
 
-
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
@@ -93,40 +149,44 @@ if __name__ == "__main__":
         print("Network is not an object detection task", file=sys.stderr)
         exit()
 
-    # Defaults
     if intrinsics.labels is None:
         with open("assets/coco_labels.txt", "r") as f:
             intrinsics.labels = f.read().splitlines()
     intrinsics.update_with_defaults()
 
+    high_size = (1280,720)
+    low_size = (320, 240)
+
     picam2 = Picamera2(imx500.camera_num)
-    config = picam2.create_preview_configuration(controls={"FrameRate": intrinsics.inference_rate}, buffer_count=12)
+    config = picam2.create_video_configuration(
+        controls={"FrameRate": intrinsics.inference_rate},
+        buffer_count=12,
+        main={"size": high_size, "format": "RGB888"},
+        lores={"size": low_size, "format": "YUV420"}
+    )
 
     imx500.show_network_fw_progress_bar()
     picam2.start(config, show_preview=False)
 
-    # if intrinsics.preserve_aspect_ratio:
-    #     imx500.set_auto_aspect_ratio()
+    if intrinsics.preserve_aspect_ratio:
+        imx500.set_auto_aspect_ratio()
 
     last_results = None
-    # TODO: should this be done in post_callback or after encoding?
-    #picam2.post_callback = collect_tags
+    asset_capture = None
 
     while True:
         last_results = parse_detections(picam2.capture_metadata())
+
         if len(last_results) > 0:
-
-            # capture image
-            filestem = time.strftime("%Y%m%d-%H%M%S")
+            logging.info(f"last_results: {last_results}")
             request = picam2.capture_request()
-            request.save("main", f"./{filestem}.jpg")
+            filestem = time.strftime("%Y%m%d-%H%M%S")
+            if not asset_capture:
+                asset_capture = Capture(filestem)
+            asset_capture.continue_or_start(request, filestem)
+            request.release()
+        else:
+            if asset_capture and asset_capture.is_capturing is True:
+                asset_capture.stop()
+                asset_capture = None
 
-            # log detections
-            i=1
-            for detection in last_results:
-                category = intrinsics.labels[int(detection.category)]
-                logging.info(f"{i} detected {category} ({detection.conf:.2f})")
-                i += i
-
-            time.sleep(1)
-    
---
+++ tinycam-cli.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get command args
+
+# case
+
+# start 
+
+# stop 
+
+# status
+
+# list images dir
+
+# tail logs
+
