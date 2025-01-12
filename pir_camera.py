7692e3bad13a91e91ee6277e5881548a5a8e74b6
Todd Ransom <toddransom@me.com>
2025-01-12 11:17:23 -0500

    rename to make name python compliant

--- pir_camera.py
+++ pir_camera.py
@@ -6,19 +6,30 @@ from picamera2 import Picamera2
 
 import RPi.GPIO as GPIO
 
-SENSOR_1_PIN = 4
- 
+SENSOR_1_PIN = 21
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s'
+)
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_1_PIN, GPIO.IN)
  
 def motion1(channel):
-    print('Movement on 1')
+    logging.info('Movement on 1')
+
+def capture_image():
+    ...
+
+def capture_video():
+    ...
  
 try:
     GPIO.add_event_detect(SENSOR_1_PIN , GPIO.RISING, callback=motion1)
 
     while True:
-        time.sleep(100)
+        time.sleep(1)
 except KeyboardInterrupt:
-    print("Finish...")
+    ...
 GPIO.cleanup()
