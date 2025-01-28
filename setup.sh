fce210055cfdd44bc106d484be622c0509f4c0de
Todd Ransom <toddransom@me.com>
2025-01-28 13:28:31 -0500

    changes to install ai version

--- setup.sh
+++ setup.sh
@@ -2,11 +2,20 @@
 echo "Install prerequisites"
 sudo apt install python3-picamera2 python3-opencv opencv-data
 
+# AI Camera
+if [$1 = "ai"]; then
+    sudo apt install imx500-all
+    sudo apt install -y python3-munkres
+fi
 
 echo "Install systemd service for TinyCam"
 mkdir -p $HOME/.config/systemd/user/
 mkdir -p $HOME/tinycam/images/
-cp tinycam.service $HOME/.config/systemd/user/
+
+if [$1="ai"]; then
+    cp ai_tinycam.service $HOME/.config/systemd/user/tinycam.service
+else
+    cp tinycam.service $HOME/.config/systemd/user/
 
 echo "Enable and start TinyCam"
 systemctl --user enable tinycam
