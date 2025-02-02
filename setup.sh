2d8ca9e0472b32a8034a5b7714b5e63d16ed5845
Todd Ransom <toddransom@me.com>
2025-02-02 10:58:28 -0500

    fix syntax errors

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "Install prerequisites"
 sudo apt install ffmpeg python3-picamera2 python3-opencv opencv-data
 
 # AI Camera
-if [$1 = "ai"]; then
+if [ $1 = "ai" ]; then
     sudo apt install -y imx500-all
     sudo apt install -y python3-munkres
 fi
@@ -12,10 +12,11 @@ echo "Install systemd service for TinyCam"
 mkdir -p $HOME/.config/systemd/user/
 mkdir -p $HOME/tinycam/images/
 
-if [$1="ai"]; then
+if [ $1="ai" ]; then
     cp ai_tinycam.service $HOME/.config/systemd/user/tinycam.service
 else
     cp tinycam.service $HOME/.config/systemd/user/
+fi
 
 echo "Enable and start TinyCam"
 systemctl --user enable tinycam
